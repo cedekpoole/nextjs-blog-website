2230acc0c1f898e7f72b22fd6b69d04e4a0581c2
Cameron Poole <110310005+cedekpoole@users.noreply.github.com>
2023-04-20 16:11:38 +0100

    set up project and add theme to sanity studio

--- sanity.config.ts
+++ sanity.config.ts
@@ -2,6 +2,7 @@ import {defineConfig, isDev} from 'sanity'
 import {visionTool} from '@sanity/vision'
 import {deskTool} from 'sanity/desk'
 import {schemaTypes} from './schemas'
+import { myTheme } from './theme'
 
 const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!;
 const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET!;
@@ -20,5 +21,6 @@ export default defineConfig({
   schema: {
     types: schemaTypes,
   },
+  theme: myTheme,
 })
 
---
+++ schemas/post.ts
@@ -0,0 +1,47 @@
+const post = {
+    title: 'Post',
+    name: 'post',
+    type: 'document',
+    fields: [
+      {
+        name: 'title',
+        title: 'Title',
+        type: 'string',
+      },
+      {
+        name: 'coverImage',
+        title: 'Cover image',
+        type: 'image',
+        options: {
+            hotspot: true,
+        }
+      },
+      {
+        name: 'slug',
+        title: 'Slug',
+        type: 'slug',
+      },
+      {
+        name: 'publishedAt',
+        title: 'Published at',
+        type: 'datetime',
+      },
+      {
+        name: 'body',
+        title: 'Body',
+        type: 'array',
+        of: [
+          {
+            type: 'block',
+          },
+        ],
+      },
+    ],
+    preview: {
+      select: {
+        title: 'title',
+      },
+    },
+  }
+  
+  export default post
