a7ab49a275cb04df74ee94b3b81272c6e7c188cf
Cameron Poole <110310005+cedekpoole@users.noreply.github.com>
2023-04-21 23:25:41 +0100

    remove escaped entity

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -6,7 +6,7 @@ const Banner = () => {
     <div className="w-5/6 mx-auto sm:flex justify-between">
         <MotionDiv duration={0.5} x={-100} className="mb-2">
         <h1 className="text-6xl sm:text-7xl font-bold mb-2">Cam's<br /> Weekly Blog</h1>
-        <p className="">Follow the journey of a small dev surfing the world wide web</p>
+        <p>Follow the journey of a small dev surfing the world wide web</p>
         </MotionDiv>
         <MotionDiv duration={0.7} x={100} className="sm:ml-2 max-w-xs sm:max-w-sm text-gray-200 sm:mt-5">Launch into Cyberspace | Testing the latest technologies | Eat, Sleep, Code, Repeat </MotionDiv>
     </div>
