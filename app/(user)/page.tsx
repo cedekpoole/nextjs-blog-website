8fc86f7d8eadebcfa88cc071645f8c9cb07e81c7
Cameron Poole <110310005+cedekpoole@users.noreply.github.com>
2023-04-26 22:35:35 +0100

    add animation to blog list section

--- app/(user)/page.tsx
+++ app/(user)/page.tsx
@@ -2,7 +2,7 @@ import { previewData } from "next/headers";
 import { groq } from 'next-sanity';
 import { client } from '@/lib/sanity.client';
 import BlogList from "@/components/BlogList";
-
+import { MotionDiv } from "@/components/MotionDiv";
 const query = groq`
   *[_type=="post"]{
     ...
@@ -15,8 +15,8 @@ export default async function Home() {
     return <div>PREVIEW MODE</div>;
   } 
   const posts = await client.fetch(query);
-  return <div>
+  return <MotionDiv duration={0.5} x={20}>
     <BlogList posts={posts}/>
-  </div>;
+  </MotionDiv>;
   }
 
